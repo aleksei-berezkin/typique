e8aa120a7c06e00e4dd9c8b50a0e3edb4309dabd
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2025-08-07 07:27:51 +0200

    Reworked integration tests to multiple TS projects

--- integration/integration.test.ts
+++ integration/integration.test.ts
@@ -7,15 +7,17 @@ import type { ChildProcess } from 'node:child_process'
 import readline from 'node:readline'
 import type ts from 'typescript/lib/tsserverlibrary.d.ts'
 
-const outputFile = path.join(import.meta.dirname, 'laim-output.css')
-const logFile = path.join(import.meta.dirname, 'tsserver-laim.log')
 
-const nameToContent = new Map<string, string>()
+const outputBasename = 'laim-output.css'
+const projectToNameToContent = new Map<string, Map<string, string>>()
 
 let h: ChildProcess
 
 test.before(async () => {
-  [outputFile, logFile].forEach(f =>
+  const outputFiles = getProjectBasenames().map( p => path.join(import.meta.dirname, p, outputBasename))
+  const logFile = path.join(import.meta.dirname, 'tsserver-laim.log');
+
+  [...outputFiles, logFile].forEach(f =>
     fs.rmSync(f, {force: true})
   )
 
@@ -33,45 +35,51 @@ test.before(async () => {
   readline.createInterface({input: h.stderr!}).on('line', (data) => {
     console.error(data)
   })
-  const openRequest: ts.server.protocol.OpenRequest = {
-    seq: 0,
-    type: 'request',
-    command: 'open' as ts.server.protocol.CommandTypes.Open,
-    arguments: {
-      file: path.join(import.meta.dirname, getTsFiles()[0]),
-      scriptKindName: 'TS',
-    },
+  for (const projectDirName of getProjectBasenames()) {
+    const openRequest: ts.server.protocol.OpenRequest = {
+      seq: 0,
+      type: 'request',
+      command: 'open' as ts.server.protocol.CommandTypes.Open,
+      arguments: {
+        file: path.join(import.meta.dirname, projectDirName, getTsBasenames(projectDirName)[0]),
+        scriptKindName: 'TS',
+      },
+    }
+    h.stdin!.write(JSON.stringify(openRequest) + '\n')
   }
-  h.stdin!.write(JSON.stringify(openRequest) + '\n')
 
-  while (!fs.existsSync(outputFile)) {
+  while (!outputFiles.every(f => fs.existsSync(f))) {
     await delay(200)
   }
   await delay(100)
 
-  let currentFile: string | undefined = undefined
-  for (const l of String(fs.readFileSync(outputFile)).split('\n')) {
-    const m = /^\/\* (src|end): ([\w/.]+) \*\/$/.exec(l)
-    if (m) {
-      if (currentFile) {
-        if (m[1] !== 'end')
-          throw new Error(`Expected 'end' but got '${l}'`)
-        currentFile = undefined
+  for (const projectDirName of getProjectBasenames()) {
+    const currentMap = new Map<string, string>()
+    let currentFile: string | undefined = undefined
+    for (const l of String(fs.readFileSync(path.join(import.meta.dirname, projectDirName, outputBasename))).split('\n')) {
+      const m = /^\/\* (src|end): ([\w/.]+) \*\/$/.exec(l)
+      if (m) {
+        if (currentFile) {
+          if (m[1] !== 'end')
+            throw new Error(`Expected 'end' but got '${l}'`)
+          currentFile = undefined
+        } else {
+          if (m[1] !== 'src')
+            throw new Error(`Expected 'src' but got '${l}'`)
+          currentFile = m[2]
+        }
       } else {
-        if (m[1] !== 'src')
-          throw new Error(`Expected 'src' but got '${l}'`)
-        currentFile = m[2]
-      }
-    } else {
-      if (currentFile) {
-        const content = nameToContent.get(currentFile)
-        nameToContent.set(currentFile, content ? `${content}\n${l}` : l)
-      } else if (l) {
-        const e = new Error(`Expected 'src' or 'end' but got '${l}'`)
-        console.error(e)
-        throw e // doesn't fail the test
+        if (currentFile) {
+          const content = currentMap.get(currentFile)
+          currentMap.set(currentFile, content ? `${content}\n${l}` : l)
+        } else if (l) {
+          const e = new Error(`Expected 'src' or 'end' but got '${l}'`)
+          console.error(e)
+          throw e // doesn't fail the test
+        }
       }
     }
+    projectToNameToContent.set(projectDirName, currentMap)
   }
 })
 
@@ -97,27 +105,43 @@ function delay(ms: number) {
 
 test('names', () => {
   assert.deepEqual(
-    new Set(nameToContent.keys()),
-    new Set(getTsFiles())
+    new Set(projectToNameToContent.keys()),
+    new Set(getProjectBasenames()),
   )
+  for (const projectDirName of getProjectBasenames()) {
+    const names = projectToNameToContent.get(projectDirName)!
+    assert.deepEqual(
+      new Set(names.keys()),
+      new Set(getTsBasenames(projectDirName)),
+    )
+  }
 })
 
-for (const cssF of getCssFiles()) {
-  test(cssF, () => {
-    const expected = String(readFileSync(path.join(import.meta.dirname, cssF))).trim()
-    const actual = nameToContent.get(cssF.replace('.css', '.ts'))
-    assert.equal(actual, expected)
-  })
+for (const projectDirName of getProjectBasenames()) {
+  for (const cssFile of getProjectBasenames().flatMap(getCssBasenames)) {
+    test(`${projectDirName}/${cssFile}`, () => {
+      const expected = String(readFileSync(path.join(import.meta.dirname, projectDirName, cssFile))).trim()
+      const actual = projectToNameToContent.get(projectDirName)!.get(cssFile.replace('.css', '.ts'))
+      assert.equal(actual, expected)
+    })
+  }
 }
 
-function getCssFiles() {
-  return fs.readdirSync(import.meta.dirname)
-    .filter(f => f.endsWith('.css') && f !== path.basename(outputFile))
+function getProjectBasenames() : string[] {
+  return fs.readdirSync(import.meta.dirname, {withFileTypes: true})
+    .filter(ent => ent.isDirectory())
+    .map(ent => ent.name)
+    // .filter(dir => dir === 'basic')
+}
+
+function getCssBasenames(projectBasename: string) {
+  return fs.readdirSync(path.join(import.meta.dirname, projectBasename))
+    .filter(f => f.endsWith('.css') && f !== outputBasename)
     // .filter(f => f.includes('constLabel'))
 }
 
-function getTsFiles() {
-  return getCssFiles().map(f => f.replace('.css', '.ts'))
+function getTsBasenames(projectBasename: string) {
+  return getCssBasenames(projectBasename).map(f => f.replace('.css', '.ts'))
 }
 
 test.run()
